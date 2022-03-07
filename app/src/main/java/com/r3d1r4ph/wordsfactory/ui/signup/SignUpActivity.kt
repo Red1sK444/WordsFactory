965d9eb7e5861bb5afde0716b0e579bf6889486c
Armen Gevorkyan <rediraph@mail.ru>
2022-03-08 01:07:33 +0700

    signup resources added

--- app/src/main/java/com/r3d1r4ph/wordsfactory/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/r3d1r4ph/wordsfactory/ui/signup/SignUpActivity.kt
@@ -1,11 +1,12 @@
-package com.r3d1r4ph.wordsfactory
+package com.r3d1r4ph.wordsfactory.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.r3d1r4ph.wordsfactory.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
