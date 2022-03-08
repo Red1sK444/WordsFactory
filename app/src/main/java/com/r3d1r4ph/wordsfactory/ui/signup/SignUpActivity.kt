8019517111f7e18644b0ef95fd9cd42d47cf60bf
Armen Gevorkyan <rediraph@mail.ru>
2022-03-08 11:51:51 +0700

    presentation layer

--- app/src/main/java/com/r3d1r4ph/wordsfactory/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/r3d1r4ph/wordsfactory/ui/signup/SignUpActivity.kt
@@ -1,12 +1,88 @@
 package com.r3d1r4ph.wordsfactory.ui.signup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.r3d1r4ph.wordsfactory.R
+import com.r3d1r4ph.wordsfactory.databinding.ActivitySignUpBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val viewBinding by viewBinding(ActivitySignUpBinding::bind, R.id.rootLayout)
+    private val viewModel by viewModels<SignUpViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        setErrorDismisses()
+        setOnClickListener()
+        setCollector()
+    }
+
+    private fun setErrorDismisses() = with(viewBinding) {
+        signUpNameTextInputEditText.addTextChangedListener {
+            signUpNameTextInputLayout.error = resources.getString(R.string.empty)
+        }
+
+        signUpEmailTextInputEditText.addTextChangedListener {
+            signUpEmailTextInputLayout.error = resources.getString(R.string.empty)
+        }
+
+        signUpPasswordTextInputEditText.addTextChangedListener {
+            signUpPasswordTextInputLayout.error = resources.getString(R.string.empty)
+        }
+    }
+
+    private fun setOnClickListener() {
+        viewBinding.signUpButton.setOnClickListener {
+            with(viewBinding) {
+                viewModel.signUp(
+                    name = signUpNameTextInputEditText.text.toString(),
+                    email = signUpEmailTextInputEditText.text.toString(),
+                    password = signUpPasswordTextInputEditText.text.toString()
+                )
+            }
+        }
+    }
+
+    private fun setCollector() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    if (uiState.openDictionaryScreen) {
+                        openDictionaryScreen()
+                    } else {
+                        setErrors(uiState.nameError, uiState.emailError, uiState.passwordError)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setErrors(
+        @StringRes nameError: Int,
+        @StringRes emailError: Int,
+        @StringRes passwordError: Int
+    ) = with(viewBinding) {
+        signUpNameTextInputLayout.error =
+            resources.getString(nameError)
+        signUpEmailTextInputLayout.error =
+            resources.getString(emailError)
+        signUpPasswordTextInputLayout.error =
+            resources.getString(passwordError)
+    }
+
+    private fun openDictionaryScreen() {
+        Toast.makeText(this, "openDictionaryScreen", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/r3d1r4ph/wordsfactory/ui/signup/SignUpViewModel.kt
@@ -0,0 +1,69 @@
+package com.r3d1r4ph.wordsfactory.ui.signup
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.r3d1r4ph.wordsfactory.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class SignUpUiState(
+    @StringRes val nameError: Int = R.string.empty,
+    @StringRes val emailError: Int = R.string.empty,
+    @StringRes val passwordError: Int = R.string.empty,
+    val openDictionaryScreen: Boolean = false
+)
+
+class SignUpViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SignUpUiState())
+    val uiState: StateFlow<SignUpUiState>
+        get() = _uiState.asStateFlow()
+
+    fun signUp(
+        name: String,
+        email: String,
+        password: String
+    ) {
+        val nameError = emptyFieldCheck(name)
+        val emailError = emailFieldCheck(email)
+        val passwordError = emptyFieldCheck(password)
+
+        viewModelScope.launch {
+            _uiState.update {
+                SignUpUiState(
+                    nameError = nameError,
+                    emailError = emailError,
+                    passwordError = passwordError,
+                    openDictionaryScreen = nameError == R.string.empty
+                            && emailError == R.string.empty
+                            && passwordError == R.string.empty
+                )
+            }
+        }
+    }
+
+    private fun emptyFieldCheck(
+        text: String
+    ): Int = if (text.isEmpty()) {
+        R.string.signup_empty_field
+    } else {
+        R.string.empty
+    }
+
+    private fun emailFieldCheck(
+        text: String
+    ): Int {
+        val empty = emptyFieldCheck(text)
+        return if (empty != R.string.empty) {
+            empty
+        } else if (!text.contains('@')) {
+            R.string.signup_not_e_mail
+        } else {
+            R.string.empty
+        }
+    }
+}
