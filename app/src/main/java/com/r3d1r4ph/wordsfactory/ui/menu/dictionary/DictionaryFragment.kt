fe20aa272ed37670d1e56e9066cafce660322e95
Armen Gevorkyan <rediraph@mail.ru>
2022-03-17 05:31:52 +0700

    added media player

--- app/src/main/java/com/r3d1r4ph/wordsfactory/ui/menu/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/r3d1r4ph/wordsfactory/ui/menu/dictionary/DictionaryFragment.kt
@@ -1,6 +1,8 @@
 package com.r3d1r4ph.wordsfactory.ui.menu.dictionary
 
 import android.annotation.SuppressLint
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.r3d1r4ph.wordsfactory.R
 import com.r3d1r4ph.wordsfactory.databinding.FragmentDictionaryBinding
+import java.io.IOException
 import java.util.*
 
 class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
@@ -16,6 +19,7 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
     private val viewBinding by viewBinding(FragmentDictionaryBinding::bind)
     private val viewModel by viewModels<DictionaryViewModel>()
     private val meaningsAdapter = MeaningsAdapter()
+    private val mediaPlayer = MediaPlayer()
 
     companion object {
         val TAG: String = DictionaryFragment::class.java.simpleName
@@ -24,12 +28,27 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        configureRecycler()
+        initView()
         setObserver()
+    }
+
+    private fun initView() {
+        configureRecycler()
 
         viewBinding.dictionarySearchTextInputLayout.setEndIconOnClickListener {
             viewModel.search(viewBinding.dictionarySearchTextInputEditText.text.toString())
         }
+
+        viewBinding.dictionaryVolumeImageButton.setOnClickListener {
+            mediaPlayer.start()
+        }
+
+        mediaPlayer.setAudioAttributes(
+            AudioAttributes
+                .Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build()
+        )
     }
 
     private fun configureRecycler() {
@@ -62,6 +81,14 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
                         it.partOfSpeech.first().uppercase(Locale.ENGLISH)
                     }${it.partOfSpeech.substring(1)}"
                     meaningsAdapter.submitList(it.meanings)
+
+                    try {
+                        mediaPlayer.reset()
+                        mediaPlayer.setDataSource(it.audio)
+                        mediaPlayer.prepareAsync()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
                 }
             }
         }
