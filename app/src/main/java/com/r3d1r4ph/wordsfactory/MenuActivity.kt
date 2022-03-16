6614ac0d57ec42b7dfec1979885918b99efa3175
Armen Gevorkyan <rediraph@mail.ru>
2022-03-16 13:54:26 +0700

    all resources added

--- app/src/main/java/com/r3d1r4ph/wordsfactory/MenuActivity.kt
+++ app/src/main/java/com/r3d1r4ph/wordsfactory/MenuActivity.kt
@@ -3,9 +3,9 @@ package com.r3d1r4ph.wordsfactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
     }
 }
